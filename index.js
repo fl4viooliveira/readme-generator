946eae70d00b8cf3fd4f7fb00a8912fb0f7c8efa
Flavio Oliveira <61277545+fl4viooliveira@users.noreply.github.com>
2023-02-15 11:39:13 +0000

    Create a questions, writeToFile and init function

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
 const fs = require("fs-extra");
-const path = require('path');
+const path = require("path");
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
-const questions = [
-
-];
+const questions = () =>
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "What is the title to this project?",
+    },
+  ]);
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+const writeToFile = (fileName, data) => {
+  const write = util.promisify(fs.writeFile);
+  data()
+    .then((data) => write(fileName, generateMarkdown(data)))
+    .then(() => console.log("Successfully wrote to your file"))
+    .catch((err) => console.error(err));
+};
 
 // function to initialize program
-function init() {
-
-}
+const init = () => writeToFile("test.md", questions() )
+// function init() {}
 
 // function call to initialize program
 init();
