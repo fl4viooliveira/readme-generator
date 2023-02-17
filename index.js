045d7db642bf275d13c671b81e37ada579f379aa
Flavio Oliveira <61277545+fl4viooliveira@users.noreply.github.com>
2023-02-17 18:52:59 +0000

    Get User Name and Repo Name from the Repo url

--- index.js
+++ index.js
@@ -12,19 +12,72 @@ const questions = () =>
       name: "title",
       message: "What is the title to this project?",
     },
+    {
+      type: "input",
+      name: "repoUrl",
+      message: "Add your repository complete link here, Exemple: 'https://github.com/<USERNAME>/<REPONAME>'"
+    },
+    // {
+    //   type: "input",
+    //   name: "github_username",
+    //   message: "What is your GitHub name?",
+    // },
+    // {
+    //   type: "input",
+    //   name: "repo_name",
+    //   message: "What's the project repo name?",
+    // },
+    // ================ test ======================
+    // {
+    //   type: "confirm",
+    //   name: "bacon",
+    //   message: "Do you like bacon?",
+    // },
+    // {
+    //   type: "input",
+    //   name: "favorite",
+    //   message: "Bacon lover, what is your favorite type of bacon?",
+    //   when(answers) {
+    //     return answers.bacon;
+    //   },
+    // },
+    // {
+    //   type: "confirm",
+    //   name: "pizza",
+    //   message: "Ok... Do you like pizza?",
+    //   when(answers) {
+    //     return !likesFood("bacon")(answers);
+    //   },
+    // },
+    // {
+    //   type: "input",
+    //   name: "favorite",
+    //   message: "Whew! What is your favorite type of pizza?",
+    //   when: likesFood("pizza"),
+    // },
   ]);
 
- // function to write README file
- const writeToFile = (fileName, quest) => {
-   const write = util.promisify(fs.writeFile);
-   quest()
-     .then((data) => write(fileName, generateMarkdown(data)))
-     .then(() => console.log("Successfully you have a amazing Readme file to add to your repo."))
-     .catch((err) => console.error(err));
- };
+function likesFood(aFood) {
+  return function (answers) {
+    return answers[aFood];
+  };
+}
 
- // function to initialize program
- const init = () => writeToFile("test.md", questions);
+// function to write README file
+const writeToFile = (fileName, quest) => {
+  const write = util.promisify(fs.writeFile);
+  quest()
+    .then((data) => write(fileName, generateMarkdown(data)))
+    .then(() =>
+      console.log(
+        "Successfully you have a amazing Readme file to add to your repo."
+      )
+    )
+    .catch((err) => console.error(err));
+};
 
- // function call to initialize program
- init()
+// function to initialize program
+const init = () => writeToFile("test.md", questions);
+
+// function call to initialize program
+init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,12 +1,41 @@
 // function to generate markdown for README
-// const generateMarkdown = (data) => `
-//   # ${data.title}
-// `;
 
-module.exports = function(data){
+let userName;
+let repoName;
+
+const repoLink = (url) => {
+  if (url) {
+    userName = url.split("/").slice(-2)[0];
+    repoName = url.split("/").pop();
+  } else {
+    userName = `Had some error on you Link input: ${url}`;
+    repoName = `Had some error on you Link input: ${url}`;
+  }
+};
+// https://github.com/fl4viooliveira/find-your-movie
+
+module.exports = function (data) {
+  repoLink(data.repoUrl)
+  console.log(data, userName, repoName);
   return `
   # ${data.title}
-`
-}
+`;
+};
 
-// module.export = generateMarkdown();
+// [![Contributors][contributors-shield]][contributors-url]
+// [![Forks][forks-shield]][forks-url]
+// [![Stargazers][stars-shield]][stars-url]
+// [![Issues][issues-shield]][issues-url]
+// [![MIT License][license-shield]][license-url]
+// [![LinkedIn][linkedin-shield]][linkedin-url]
+//   # ${data.title}
+// [contributors-shield]: https://img.shields.io/github/contributors/${data.github_username}/${data.repo_name}.svg?style=for-the-badge
+// [contributors-url]: https://github.com/${data.github_username}/${data.repo_name}/graphs/contributors
+// [forks-shield]: https://img.shields.io/github/forks/${data.github_username}/${data.repo_name}.svg?style=for-the-badge
+// [forks-url]: https://github.com/${data.github_username}/${data.repo_name}/network/members
+// [stars-shield]: https://img.shields.io/github/stars/${data.github_username}/${data.repo_name}.svg?style=for-the-badge
+// [stars-url]: https://github.com/${data.github_username}/${data.repo_name}/stargazers
+// [issues-shield]: https://img.shields.io/github/issues/${data.github_username}/${data.repo_name}.svg?style=for-the-badge
+// [issues-url]: https://github.com/${data.github_username}/${data.repo_name}/issues
+// [license-shield]: https://img.shields.io/github/license/${data.github_username}/${data.repo_name}.svg?style=for-the-badge
+// [license-url]: https://github.com/${data.github_username}/${data.repo_name}/blob/master/LICENSE.txt
